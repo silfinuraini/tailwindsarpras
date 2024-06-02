aead42ce753e9e05092706918ef129212b53b437
unknown <silfinuraini0@gmail.com>
2024-06-02 10:26:23 +0700

    first commit

---
+++ src/js/theme.js
@@ -0,0 +1,11 @@
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function ()
+{
+    if (darkToggle.checked) {
+        html.classList.add('dark');
+    } else {
+        html.classList.remove('dark');
+    }
+})
